84585764575c2facaf037e7dc274de12c65d5026
Yohai Wiener <yohaiwiener@gmail.com>
2025-02-16 16:13:13 +0200

    move out of chapter dirs for simplicty

---
+++ monkey/token/token.go
@@ -0,0 +1,65 @@
+package token
+
+type TokenType string
+
+const (
+	ILLEGAL = "ILLEGAL" // an illegal char we do not know how to parse
+	EOF     = "EOF"     // end of file. tells the parser it can stop
+
+	// Identifiers + literals
+	IDENT = "IDENT" // variable names
+	INT   = "INT"   // integers
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	EQ       = "=="
+	NOT_EQ   = "!="
+
+	LT = "<"
+	GT = ">"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
